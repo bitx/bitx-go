4f1bed210dc3fad19ae1a4341e79bbc1c46dd4b8
Timothy Stranex <timothy@Timothys-MacBook-Pro.local>
2014-08-24 23:18:57 +0200

    Fix test.

--- bitx_test.go
+++ bitx_test.go
@@ -3,5 +3,8 @@ package bitx
 import "testing"
 
 func TestExample(t *testing.T) {
-	t.Errorf("testing the test 2")
+	c := NewClient("test", "test")
+	if c == nil {
+		t.Errorf("Expected valid client, got: %v", c)
+	}
 }
